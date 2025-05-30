230eb7906f82c8b1e0f7545ce5926a5b7a77ddfa
v_v_moon <vladimir.mun.47@gmail.com>
2025-05-31 01:01:44 +0500

    build

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,27 +1,38 @@
-import Post from '@/app/components/Post'
+import Post from '@/app/components/Post';
 
-export async function generateMetadata({ params, searchParams }) {
-    const post = await fetchData(params.id)
-    return {
-        title: post.title,
-        description: post.body
-    }
+export async function generateMetadata({ params }) {
+  const post = await fetchData(params.id);
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
+
+export async function generateStaticParams() {
+  // Получаем список всех постов
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  // Возвращаем массив параметров
+  return posts.map(post => ({
+    id: post.id.toString(), // Преобразуем ID в строку
+  }));
 }
 
 async function fetchData(id) {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
   const result = await res.json();
   return result;
 }
 
-const PagePost = async ({params: { id } }) => {
-const post = await fetchData(id);
+const PagePost = async ({ params: { id } }) => {
+  const post = await fetchData(id);
 
-    return (
-        <div className='post'>
-            <Post post={post} />
-        </div>
-    )
-}
+  return (
+    <div className="post">
+      <Post post={post} />
+    </div>
+  );
+};
 
 export default PagePost;
